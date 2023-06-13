f65100a4c448244f4d83a3de9ce3e128a91b365e
Subhransu Bhattacharjee <bhattacharjeesekharsubhransu1@gmail.com>
2023-06-13 22:50:00 +1000

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 # Code by 1ssb: https://github.com/1ssb/Image-Randomizer
 
 import os
+import shutil
+import numpy as np
 from PIL import Image
 import torch
 from torchvision import transforms
@@ -62,13 +64,7 @@ def copy_diverse_images(source_path, dest_path, k, eval, batch):
         gamma = 1.0
         kT = 1.0 + i*dt
         source_distribution += -gamma * source_distribution * dt + np.sqrt(2 * gamma * kT * dt) * torch.randn_like(source_distribution)
-
-        sample_indices = torch.randperm(len(source_distribution))[:5*k]
-        sample_distribution = source_distribution[sample_indices]
-        m = 0.5 * (sample_distribution + source_distribution)
-        js_distance = 0.5 * (torch.sum(sample_distribution * torch.log(sample_distribution / m)) + torch.sum(source_distribution * torch.log(source_distribution / m)))
-        
-        best_subset = select_diverse_images(sample_distribution, k, batch)
+        best_subset = select_diverse_images(source_distribution, k, batch)
         best_images.extend(best_subset.tolist())
         
     for i in best_images:
@@ -77,6 +73,6 @@ def copy_diverse_images(source_path, dest_path, k, eval, batch):
 source_path = '/path/to/source/images'
 dest_path = '/path/to/destination/folder'
 k = 10
-eval = 50
+evals = 50
 batch = 50
-copy_diverse_images(source_path, dest_path, k, eval)
+copy_diverse_images(source_path, dest_path, k, evals, batch)
